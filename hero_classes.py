92781a3825ff1e27fa3516c9786f7b5b030dda28
kostya-god <62570769+kostya-god@users.noreply.github.com>
2021-01-09 14:03:29 +0300

    Исправил опечатку

--- hero_classes.py
+++ hero_classes.py
@@ -14,28 +14,28 @@ class Wizard(Attacker):
         self._attack = 125
         self._experience = 0
         self._name = name
-        self._inportance = 1.5
+        self._importance = 1.5
 class Healer(Attacker):
     def __init__(self, name):
         self._health = 50
         self._attack = 50
         self._experience = 0
         self._name = name
-        self._inportance = 0.5
+        self._importance = 0.5
 class Jagernaut(Attacker):
     def __init__(self, name):
         self._health = 125
         self._attack = 60
         self._experience = 0
         self._name = name
-        self._inportance = 1.15
+        self._importance = 1.15
 class Imba(Attacker):
     def __init__(self, name):
         self._health = 1000
         self._attack = 1000
         self._experience = 0
         self._name = name
-        self._inportance = 5
+        self._importance = 5
 
 
 def attack(self, target):
